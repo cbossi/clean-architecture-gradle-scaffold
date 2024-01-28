50fa2f000f411040648b88ffa042a7f6035b264b
Christian Bosshard <bosshardchr@gmail.com>
2024-01-28 19:36:49 +0100

    Implement test case.

--- customer/src/test/kotlin/ch/cbossi/cags/customer/CustomerTest.kt
+++ customer/src/test/kotlin/ch/cbossi/cags/customer/CustomerTest.kt
@@ -1,14 +1,19 @@
 package ch.cbossi.cags.customer
 
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+
 class CustomerTest {
 
+    @Test
     fun test() {
-        val customer: Customer = Customer("Test Customer")
         val repository: CustomerRepository = CustomerRepositoryImpl()
         val service = CustomerService(repository)
 
-        service.loadCustomers()
-        println(customer)
+        val customers = service.loadCustomers()
+
+        assertTrue(customers.size == 2)
+        assertTrue(customers.containsAll(setOf(Customer("John Doe"), Customer("Jane Smith"))))
     }
 
 }
